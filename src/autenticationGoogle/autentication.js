a6f7d8cd8d2677e96c6aeceae18f46fcd36c33e9
thiago <desenvolvedor.nunes@gmail.com>
2021-11-17 05:36:03 -0300

    adiciona rotas de login conta google

--- src/autenticationGoogle/autentication.js
+++ src/autenticationGoogle/autentication.js
@@ -1,8 +1,27 @@
 const express = require('express')
-const app = express.Router()
+const session = require('express-session')
+const app = express()
 const passport = require('passport')
 require('./auth')
 
+app.use(session({ secret: 't423h5a213g7898i04s' }))
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/home',
+  failureRedirect: '/'
+}))
+
+function verifyResponse (req, res, next) {
+  req.user.id ? next() : res.status(401).send()
+}
+
+app.get('/home', verifyResponse, (req, res) => {
+  console.log(req.user)
+  res.send('foi-se')
+})
 
 module.exports = app
