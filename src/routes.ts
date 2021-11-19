339206f72d614397978ce67682d2b7c1758463b8
thiago <desenvolvedor.nunes@gmail.com>
2021-11-19 11:02:25 -0300

    cria rota de login

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,46 @@
 import express from 'express'
 import validaRota from './controller/cadastro/validaRota'
+import CryptoJS from 'crypto-js'
+import dataBase from './dataBase/dataBase'
+import { sign } from 'jsonwebtoken'
 
 const routers = express.Router()
 
 routers.get('/', (req, res) => {
-  res.render('./usuario/index.ejs')
+  res.render('./usuario/login.ejs')
 })
 
 routers.get('/cadastro', (req, res) => {
-  res.render('./usuario/cadastro.ejs', { err: {} })
+  res.render('./usuario/cadastro.ejs', { err: {}, msgInfor: '', erroSistema: {} })
 })
 
 routers.post('/cadastro', validaRota.validar_campos, (req, res) => {
   validaRota.validar_rota(req, res)
 })
 
-// routers.post('/login', (req, res) => {
-//   dataBase.query('insert into usuarios ')
-// })
+interface DadosLogin {
+  email?: string
+  senha?: string
+  id?: string
+}
+
+routers.post('/login', (req, res) => {
+  const { email, senha }: DadosLogin = req.body
+  const senhaCriptografada: string = CryptoJS.MD5(senha).toString()
+
+  dataBase.query('select * from usuarios where email = ? and senha = ?', [email, senhaCriptografada], (err, result) => {
+    if (!err && result) {
+      const { id }: DadosLogin = result[0]
+      const token = sign(id, process.env.SECRET_JWT)
+      res.header('token', token)
+      res.redirect('/home')
+    }
+    res.status(500).send()
+  })
+})
+
+routers.get('/home', (req, res) => {
+  console.log(req.user)
+})
 
 export default routers
